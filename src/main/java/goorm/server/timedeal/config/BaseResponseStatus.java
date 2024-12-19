1a495416159ce7470d00e5aa928d071008586f8b
hyeonjeong-ko <hyeonkjjang@gmail.com>
2024-12-19 16:47:23 +0900

    ✨ feat: Add and modify response status codes
    - 타임딜 API를 위한 상태 코드 추가
    - 기존 상태 코드 중복 및 명확성 개선

--- src/main/java/goorm/server/timedeal/config/BaseResponseStatus.java
+++ src/main/java/goorm/server/timedeal/config/BaseResponseStatus.java
@@ -6,6 +6,10 @@ import lombok.Getter;
 public enum BaseResponseStatus {
 
 	SUCCESS(true, 1000, "요청에 성공하였습니다."),
+	ERROR(false, 2000, "서버 오류가 발생했습니다."),
+	FORBIDDEN(false, 2003, "권한이 없는 유저의 접근입니다.");
+
+	/*
 	REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
 	INVALID_USER_JWT(false, 2003, "권한이 없는 유저의 접근입니다."),
 	USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
@@ -20,7 +24,7 @@ public enum BaseResponseStatus {
 	MODIFY_FAIL_USERNAME(false, 4014, "유저네임 수정 실패"),
 	PASSWORD_ENCRYPTION_ERROR(false, 4011, "비밀번호 암호화에 실패하였습니다."),
 	PASSWORD_DECRYPTION_ERROR(false, 4012, "비밀번호 복호화에 실패하였습니다.");
-
+	*/
 	private final boolean isSuccess;
 	private final int code;
 	private final String message;
