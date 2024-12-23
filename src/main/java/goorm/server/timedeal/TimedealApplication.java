74a411e29fe001c89cebeb41bc45ccce8fb58f55
hyeonjeong ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2024-12-23 11:53:30 +0900

    Revert "Feature/active time deal display"

--- src/main/java/goorm/server/timedeal/TimedealApplication.java
+++ src/main/java/goorm/server/timedeal/TimedealApplication.java
@@ -2,7 +2,6 @@ package goorm.server.timedeal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class TimedealApplication {
--- src/main/java/goorm/server/timedeal/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package goorm.server.timedeal.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
