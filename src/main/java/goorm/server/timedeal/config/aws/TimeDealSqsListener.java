4cf8883b6eebd8e436dc404a4860483f5802bb64
hyeonjeong-ko <hyeonkjjang@gmail.com>
2024-12-23 22:37:14 +0900

    🔧: Add logging to TimeDealSqsListener for better traceability
    - Added logging to SQS listener to track message processing and errors
    - Improved visibility of SQS message handling flow for debugging purposes

--- src/main/java/goorm/server/timedeal/config/aws/TimeDealSqsListener.java
+++ src/main/java/goorm/server/timedeal/config/aws/TimeDealSqsListener.java
@@ -1,20 +1,14 @@
 package goorm.server.timedeal.config.aws;
 
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import goorm.server.timedeal.dto.SQSTimeDealDTO;
-import goorm.server.timedeal.model.TimeDeal;
 import goorm.server.timedeal.service.NotificationService;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import software.amazon.awssdk.services.sqs.endpoints.internal.Value;
 
 @Slf4j
 @Component
@@ -33,9 +27,12 @@ public class TimeDealSqsListener {
 				? message.substring(1, message.length() - 1).replace("\\\"", "\"")
 				: message;
 
+			log.info("decodedMessage = " + decodedMessage);
+
 
 			SQSTimeDealDTO timeDealDTO = objectMapper.readValue(decodedMessage, SQSTimeDealDTO.class);
 			log.info("타임딜 이벤트 정보 변경이 일어났습니다: " + timeDealDTO.getTimeDealId());
+			log.info("타일딜 이벤트 정보 변경이 일어난 메시지 타입: " + timeDealDTO.getMessageType());
 
 			// 메시지를 NotificationService 로 전달하여 Thymeleaf 로 알림 처리
 			notificationService.sendUpdateToThymeleaf(timeDealDTO);
